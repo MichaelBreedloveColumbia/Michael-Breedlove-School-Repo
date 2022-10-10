34d12561f9430bac4d628c87f049e53edb337399
MichaelBreedloveColumbia <112978110+MichaelBreedloveColumbia@users.noreply.github.com>
2022-10-10 17:40:26 -0500

    Add files via upload

---
+++ Week 5 Methods/FisherYates/FisherYates/Program.cs
@@ -0,0 +1,29 @@
+﻿using FisherYates;
+
+List<int> Numbers = new List<int>();
+Shuffle shuffler = new Shuffle();
+
+for (int i = 1; i <= 10; i++)
+{
+    Numbers.Add(i);
+}
+
+PrintNumbers();
+Console.WriteLine("\n\nShuffling the list.\n");
+Numbers = shuffler.ShuffleList(Numbers);
+PrintNumbers();
+Console.WriteLine("\n\nPress any key to exit.");
+Console.ReadKey();
+
+void PrintNumbers()
+{
+    Console.WriteLine("The list is currently: ");
+    for(int i = 0; i < Numbers.Count; i++)
+    {
+        Console.Write(Numbers[i] + " ");
+        if (i == Numbers.Count)
+        {
+            Console.WriteLine("\n");
+        }
+    }
+}
---
+++ Week 5 Methods/FisherYates/FisherYates/Shuffle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FisherYates
+{
+    public class Shuffle
+    {
+        public List<int> ShuffleList(List<int> Original)
+        {
+            List<int> New = Original;
+
+            for (int i = New.Count - 1; i >= 0; i--)
+            {
+                int j = GetRandomNumber(i);
+                int temporary = New[j];
+                New[j] = Original[i];
+                New[i] = temporary;
+            }
+
+            return New;
+        }
+
+        int GetRandomNumber(int i)
+        {
+            Random rand = new Random();
+            return rand.Next(i + 1);
+        }
+    }
+}
