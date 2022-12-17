4ce56ce6c30baf517e86d28ac5abe714911e623e
MichaelBreedloveColumbia <112978110+MichaelBreedloveColumbia@users.noreply.github.com>
2022-12-16 22:09:39 -0600

    Add files via upload

--- Week 12 Methods/SearchDemo/SearchDemo/SearchExample.cs
+++ Week 12 Methods/SearchDemo/SearchDemo/SearchExample.cs
@@ -88,7 +88,16 @@ namespace SearchDemo
 
             ForegroundColor = ConsoleColor.White;
 
-            WriteLine("The linear search is the simplest of the search methods, as it merely cycles through the given collection until it finds its target value. One would think that the linear search's incredible simplicity would make it the quickest method, though this would be a false assumption; linear searching is often the slowest method, with the only exception being very small collections.\nLinear searches compensate for this by being universal, as they are the only search method which doesn't require a sorted collection.\nThe absolute worst-case runtime for a linear search is O(n).\nThe average runtime for a linear search is O(n/2).\nThe best-case runtime for a linear search is O(1).\nPress any key to continue.");
+            WriteLine("The linear search is the simplest of the search methods, as it merely cycles through the given collection until it finds its target value. One would think that the linear search's incredible simplicity would make it the quickest method, though this would be a false assumption; linear searching is often the slowest method, with the only exception being very small collections.\nLinear searches compensate for this by being universal, as they are the only search method which doesn't require a sorted collection.\nThe absolute worst-case runtime for a linear search is O(n).\nThe average runtime for a linear search is O(n/2).\nThe best-case runtime for a linear search is O(1).\nIn pseudocode, the linear search can be expressed as follows:");
+            ForegroundColor = ConsoleColor.Blue;
+            WriteLine(@"Begin
+1) Set i = 0
+2) If Li = T, go to line 4
+3) Increase i by 1 and go to line 2
+4) If i < n then return i
+End");
+            ForegroundColor = ConsoleColor.White;
+            WriteLine("Press any key to continue.");
             ReadKey();
         }
 
@@ -139,7 +148,22 @@ namespace SearchDemo
 
             ForegroundColor = ConsoleColor.White;
 
-            WriteLine("The binary search requires a sorted data set, making it less universal than a linear search. However, it is still powerful.\nThe binary search starts by selecting the value in the middle of the collection. If this value is equal to the target value, the search is completed. If it is not, it compares the current value with the target value. If the target value is greater, it moves to the right side of its current position in the collection, otherwise it moves to the left.\nThe binary search repeats this process indefinitely until it has located its target value.\nThe absolute worst-case runtime for a binary search is O(log2n).\nThe average runtime for a binary search is O(log2n).\nThe best-case runtime for a binary search is O(1).\nPress any key to continue.");
+            WriteLine("The binary search requires a sorted data set, making it less universal than a linear search. However, it is still powerful.\nThe binary search starts by selecting the value in the middle of the collection. If this value is equal to the target value, the search is completed. If it is not, it compares the current value with the target value. If the target value is greater, it moves to the right side of its current position in the collection, otherwise it moves to the left.\nThe binary search repeats this process indefinitely until it has located its target value.\nThe absolute worst-case runtime for a binary search is O(log2n).\nThe average runtime for a binary search is O(log2n).\nThe best-case runtime for a binary search is O(1).\nIn pseudocode, the binary search can be expressed as follows:");
+            ForegroundColor = ConsoleColor.Blue;
+            WriteLine(@"function binary_search(A, n, T) is
+    L := 0
+    R := n − 1
+    while L ≤ R do
+        m := floor((L + R) / 2)
+        if A[m] < T then
+            L := m + 1
+        else if A[m] > T then
+            R := m − 1
+        else:
+            return m
+    return unsuccessful");
+            ForegroundColor = ConsoleColor.White;
+            WriteLine("Press any key to continue.");
             ReadKey();
         }
 
@@ -191,7 +215,41 @@ namespace SearchDemo
 
             ForegroundColor = ConsoleColor.White;
 
-            WriteLine("The interpolation search, like the binary search, requires a sorted collection. Additionally, that collection must be uniformly distributed.\nIt is effectively a more efficient binary search, at the cost of requiring more setup. Whereas binary search starts in the middle of the collection every time, interpolation search starts in different locations based on the value being searched for. If the value is very close to the last item in the collection, interpolation search will start closer to the end of the collection. If it is closer to the beginning, interpolation will start near the beginning.\nThis makes interpolation searches very swift and efficient, though, again, they require the most setup of the three primary search methods.\nThe absolute worst-case runtime for an interpolation search is O(n).\nThe average runtime for an interpolation search is O(log log N).\nThe best-case runtime for an interpolation search is O(log log N).\nPress any key to exit.");
+            WriteLine("The interpolation search, like the binary search, requires a sorted collection. Additionally, that collection must be uniformly distributed.\nIt is effectively a more efficient binary search, at the cost of requiring more setup. Whereas binary search starts in the middle of the collection every time, interpolation search starts in different locations based on the value being searched for. If the value is very close to the last item in the collection, interpolation search will start closer to the end of the collection. If it is closer to the beginning, interpolation will start near the beginning.\nThis makes interpolation searches very swift and efficient, though, again, they require the most setup of the three primary search methods.\nThe absolute worst-case runtime for an interpolation search is O(n).\nThe average runtime for an interpolation search is O(log log N).\nThe best-case runtime for an interpolation search is O(log log N).\nIn pseudocode, the interpolation search can be expressed as follows:");
+            ForegroundColor = ConsoleColor.Blue;
+            WriteLine(@"
+A → Array list
+N → Size of A
+X → Target Value
+
+Procedure Interpolation_Search()
+
+   Set Lo  →  0
+   Set Mid → -1
+   Set Hi  →  N-1
+
+   While X does not match
+
+      if Lo equals to Hi OR A[Lo] equals to A[Hi]
+         EXIT: Failure, Target not found
+      end if
+
+      Set Mid = Lo + ((Hi - Lo) / (A[Hi] - A[Lo])) * (X - A[Lo]) 
+
+      if A[Mid] = X
+         EXIT: Success, Target found at Mid
+      else 
+         if A[Mid] < X
+            Set Lo to Mid+1
+         else if A[Mid] > X
+            Set Hi to Mid-1
+         end if
+      end if
+   End While
+
+End Procedure");
+            ForegroundColor = ConsoleColor.White;
+            WriteLine("Press any key to exit.");
             ReadKey();
         }
 
